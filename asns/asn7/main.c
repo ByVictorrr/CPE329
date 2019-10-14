3d85138d438c0571a4105e5a51069bbddee12508
Victor Delaplaine <vdelaplaines@aol.com>
2019-10-14 07:32:11 -0700

    autoadd

--- asns/asn7/main.c
+++ asns/asn7/main.c
@@ -1,5 +1,5 @@
 #include "msp.h"
-
+#include "delay.h"
 // Using UCB0 (port 1)
 #define SCLK BIT5
 #define SIMO BIT6
@@ -8,7 +8,7 @@
 // using port 3
 #define _CS BIT2
 
-#define _BIT(x) (BIT0 << x)
+#define _BIT(x) (uint16_t)(1 << x)
 
 #define GAIN _BIT(13)
 #define SHDN _BIT(12)
@@ -26,10 +26,14 @@ void init_SPI(){
 	P1->SEL1 &= ~(SCLK | SIMO | SOMI);
 	// Step 4 - clears software rst
 	EUSCI_B0 -> CTLW0 &= ~EUSCI_B_CTLW0_SWRST;
+
+
 	// Step 5 - enable interrupt (for transmitting)	
-	//EUSCI_B0 -> CTLW0 ->IE = EUSCI_B_IE_RXIE; // enable interrupts
+	//EUSCI_B0 -> CTLW0 ->IE = EUSCI_B_IE_RXIE; // enable interruptsP3->DIR |= _CS;
 	//NVIC->ISER[0] = (1 << EUSCIB_IRQn); // enable on lookup table
 	//__enable_irq(); // enable globals
+	// Step 6 - mode 0,0
+	EUSCI_B0 -> CTLW0 &= ~(EUSCI_B_CTLW0_CKPH | EUSCI_B_CTLW0_CKPL);
 }
 /**
  * main.c
@@ -38,10 +42,10 @@ void init_SPI(){
 
 void send_to_DAC(uint16_t out_voh){
 	// Step 0 - sep bytes
-	uint8_t loByte = out_voh & 0xFF, highByte = (out_voh >> 8) & 0xFF;
+	uint16_t loByte = out_voh & 0xFF, highByte = (out_voh >> 8) & 0x0F;
 	// Step 1 - set up _CS
 	P3->OUT &= ~_CS;
-	P3->DIR |= _CS;
+
 	// Step 2 - send the high byte first(put high byte in buffer)
 	EUSCI_B0 ->TXBUF = highByte;
 	// Step 3 - wait for Tx flag when tx buffer empty
@@ -49,7 +53,7 @@ void send_to_DAC(uint16_t out_voh){
 	// Step 3.1 put more data in the buffer
 	EUSCI_B0 ->TXBUF = loByte;
 	// Step 4 - clear the rx flag before using
-	EUSCI_B0 ->IFG &= ~EUSCI_B_IFG_RXIFG;
+	//EUSCI_B0 ->IFG &= ~EUSCI_B_IFG_RXIFG;
 	// Step 5 - use  the recieve flag to indicate when we sent the first 8 bits
 	while(!(EUSCI_B0 ->IFG & EUSCI_B_IFG_RXIFG));
 	// Step 6 - after transmitting 
@@ -61,12 +65,21 @@ void send_to_DAC(uint16_t out_voh){
 	
 void main(void)
 {
-
+    P3->DIR |= _CS;
+    set_clk("SMCLK");
+    set_DCO(12);
 	// Step 1 - init SPI
 	init_SPI();
-	uint16_t data = 0;
-	data |= GAIN | SHDN | 0xFFF;
+	uint16_t data[2] = { GAIN | SHDN | 0x0FFF, GAIN|SHDN| 0x0012};
+	int i=0;
 	// Step 2 - send data
-	send_to_DAC(data);
+	while(1){
+	    if (i==0)
+	        i=1;
+	    else
+	        i=0;
+
+	    send_to_DAC(data[0]);
+	}
 
 }
