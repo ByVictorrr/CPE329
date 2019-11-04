0e9da41e7e209c22d9c3e7d7f584f5f67775da70
Victor Delaplaine <vdelaplaines@aol.com>
2019-11-04 06:21:34 -0800

    recent peak to peak working

--- proj/proj3/main.c
+++ proj/proj3/main.c
@@ -8,21 +8,21 @@
 #define F_INPUT 3000000
 #define SAMPLE_RATE 4
 #define SIZE 10000
+#define MAX_SAMPLE 1000
 
 float arr[SIZE];
 
 typedef enum{FALSE, TRUE} bool;
 
 int low, high; // for high and low values
+
 bool isFirstSample = TRUE;
 int sample_count = 0;
 
-int f_wave = 0;
+int f_wave = 1000;
 int ta0_ifg_overflows = 0;
 
-
-
-
+int haha = 0;
 
 
 void barGraph(float voltage){
@@ -31,7 +31,6 @@ void barGraph(float voltage){
 	for (i=0; i < bars; i++)
 		sendCharUART('|');
 }
-
 
 void printAll(int dc, int RMS, int AVG, int PP){
 
@@ -60,7 +59,12 @@ int get_sample_count(int f_wave, int F_input, int SAMPLERATE){
 }
 
 void ADC14_IRQHandler(){
-    arr[sample_count++] = ADC14->MEM[22];
+    if (high < ADC14->MEM[22]){
+        high = ADC14->MEM[22];
+    }
+    if (low > ADC14->MEM[22]){
+        low = ADC14->MEM[22];
+    }
 }
 
 // For flag overflows
@@ -74,7 +78,23 @@ void TA0_0_IRQHandler(){
 	static int TA0_R_i = 0; // inital TAXR
 	int TA0_R_f;
     uint64_t delta_TA0;
-	// cond 1 - there is a change in the overflows
+	// step 1 - clear flag
+	TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
+	//step 2 - set the adc run
+	ADC14-> CTL0 |= ADC14_CTL0_SC | ADC14_CTL0_ENC;
+	int var;
+	// step 3 - ccr0 + 1/
+	var = 50+ 1.0/(f_wave) *( F_INPUT * 0.001);
+	TIMER_A0->CCR[0] +=var;
+
+	// step 4 - increade counter
+	if(sample_count < MAX_SAMPLE){
+	    sample_count++;
+	}else{
+	    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIE;
+	    TIMER_A0->CTL &= ~TIMER_A_CTL_IE;
+	}
+	/*
 	if (ta0_ifg_overflows != 0){
 	    // Step 1 - get difference in cycles with overflow
 	    delta_TA0= ((TA0_R_f = TIMER_A0->R)+(0xffff)*ta0_ifg_overflows - TA0_R_i);
@@ -87,6 +107,8 @@ void TA0_0_IRQHandler(){
 	f_wave = F_INPUT/delta_TA0;
 	//Step 3 - get initial rx
 	TA0_R_i = TA0_R_f;
+	*/
+
 }
 
 
@@ -102,10 +124,14 @@ void init_TA0(){
 						| TIMER_A_CCTLN_SCS // TODO: not sure if enable synchrounous
 						| TIMER_A_CCTLN_CCIE; // enable interupts
 	TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG; // clear interrupt flag
+
+	// step 4 - initalize ccr0
+	TIMER_A0->CCR[0] = 50 + (1.0/(f_wave)* 3000000.0 * 0.001);
+
 	// step 3 - enable interrupts
 	NVIC->ISER[0] = (1 << (TA0_N_IRQn & 0x1F)); // ifg flag
 	NVIC->ISER[0] = (1 << (TA0_0_IRQn & 0x1F)); // ccr1 interupts
-
+	TIMER_A0->R = 0;
 }
 
 
@@ -139,39 +165,37 @@ void main(void){
 	// step 0 - init timer a0 flag
 	init_UART();
 	init_ADC14();
+    set_clk("SMCLK");
+    set_DCO(F_INPUT/1000000);
 
 	init_TA0();
-	set_clk("SMCLK");
-	set_DCO(F_INPUT/1000000); //3mhz
 	// step 1 - configure ctl reg
 	__enable_irq();
 
-	f_wave = 86;
+	//f_wave = 200;
    // printAll(3,2,2, 2);
     float high_v, low_v;
     float Vpp;
-    low = 30000;
-    high = 0;
-    ADC14->CTL0 |= ADC14_CTL0_SC;
-    int var = get_sample_count(500,3*pow(10,6), 16);
+    low = 30000; // Low equavilent of 3V
+    high = 0; // High equivalent o 0v
     sample_count = 0;
-    //memset(arr, 0, SIZE);
 	while(1){
 
-	    ADC14->CTL0 |= ADC14_CTL0_SC;
-	    if (sample_count == var){
-	        bubbleSort(arr,sample_count);
-	        high_v = calibrated_voltage(arr[sample_count]);
+	    if (sample_count >= MAX_SAMPLE){
+
+	        TIMER_A0 -> CCTL[0] &= ~TIMER_A_CCTLN_CCIE;
+	        high_v = calibrated_voltage(high);
+            low_v = calibrated_voltage(low);
 	        send_float_UART(high_v);
 	        sendCharUART('\n');
-	        low_v = calibrated_voltage(arr[0]);
 	        send_float_UART(low_v);
 	        sendCharUART('\n');
 	        Vpp = high_v - low_v;
-	        isFirstSample = TRUE;
 	        sample_count = 0;
 	        low = 30000;
 	        high = 0;
+	        TIMER_A0 -> R = 0;
+	        TIMER_A0 -> CCTL[0] |= TIMER_A_CCTLN_CCIE;
 
 	    }
 	}
